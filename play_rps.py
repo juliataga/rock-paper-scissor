0cb93ca3c16530192bb1558cc53b2ef5985682e1
juliataga <juliataga22@gmail.com>
2025-06-22 03:19:27 +0530

    Deleted Unwanted Files & Refactored

--- play_rps.py
+++ play_rps.py
@@ -1,38 +1,59 @@
 import random
 
-print("Winning rules of the game ROCK PAPER SCISSORS are:")
-print(" ")
-print("Rock vs Paper -> Paper wins \nRock vs Scissors -> Rock wins \nPaper vs Scissors -> Scissors wins")
-print(" ")
-print("Enter your choice \n1 - Rock \n2 - Paper \n3 - Scissors")
+ROCK = 'r'
+PAPER = 'p'
+SCISSORS = 's'
 
-rock = 1
-paper = 2
-scissors = 3
+emojis = {ROCK : '🪨', SCISSORS : ' ✂️', PAPER : '📃'}
+choices = tuple(emojis.keys()) 
+computer_score = 0
+your_score = 0
 
-your_choice = int(input("Enter your choice: "))
-
-if your_choice == rock:
-    print("User choice is: Rock")
-elif your_choice == paper:
-    print("User choice is: Paper")
-elif your_choice == scissors:
-    print("User choice is: Scissors")
-else:
-    print("Invalid Entry")
+def get_user_choice():
+    while True:
+        user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid Choices")
 
 
-print("Now it's Computer's Turn...")
-computer_choice = random.randint(1,3)
+def declare_choices(user_choice,computer_choice):
+        print(f"You choose {emojis[user_choice]}")
+        print(f"Computer chose {emojis[computer_choice]}")
 
-if computer_choice == rock:
-    print("Computer choice is: Rock")
-elif computer_choice == paper:
-    print("Computer choice is: Paper")
-elif computer_choice == scissors:
-    print("Computer choice is: Scissors")
-else:
-    print("Invalid Entry")
 
-print("")
+def determine_winner(user_choice,computer_choice):
+    global your_score, computer_score
+    if user_choice == computer_choice:
+        print("Its a tie!")
+        your_score += 1
+        computer_score += 1
 
+    elif (
+        (user_choice == ROCK and computer_choice == SCISSORS) or 
+        (user_choice == SCISSORS and computer_choice == PAPER) or 
+        (user_choice == PAPER and computer_choice == ROCK)):
+        print("You win")
+        your_score += 1
+
+    else:
+        print("Computer Wins")
+        computer_score += 1
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = random.choice(choices)
+        declare_choices(user_choice,computer_choice)
+        determine_winner(user_choice,computer_choice)
+
+
+        should_continue = input("do you want to continue? (y/n): ").lower()
+
+        if should_continue == 'n':
+            print("Thank you for playing")
+            print(f'you scored {your_score} and computer scored {computer_score}')
+            break
+        
+play_game()
--- try.py
+++
@@ -1,2 +0,0 @@
-print("Hi, i am trying to pull before coding on this")
-print("great, were you able to do so?")
